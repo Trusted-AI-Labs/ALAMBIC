b195152fe5d3cee0fde7ee552ffa8396ae7545e3
Charlotte Nachtegael <charlottenachtegael@hotmail.com>
2023-01-11 14:59:37 +0100

    programmatical celery to launch accelerate

--- alambic/celery.py
+++ alambic/celery.py
@@ -7,3 +7,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alambic.settings')
 app = Celery('alambic')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+if __name__ == "__main__":
+    worker = app.Worker(pool = "solo", loglevel = "INFO")
+    worker.start()
