507374fcff07df14f5acff0d510572ecb55fcf12
Charlotte Nachtegael <charlottenachtegael@hotmail.com>
2023-06-07 11:56:20 +0200

    Update form deep learner

--- alambic_app/forms/task/classification/DeepLearningTransformers.py
+++ alambic_app/forms/task/classification/DeepLearningTransformers.py
@@ -11,15 +11,63 @@ class DeepLearningClassification(CrispyWizardStep):
     )
 
     learning_rate = forms.FloatField(
-        default = 1e-5
+        min_value=0,
+        max_value=1,
+        initial = 1e-5,
+        required=True,
+        help_text='Learning rate of the model',
+        widget=forms.NumberInput(
+            attrs={
+                'theme': 'material',
+                'data-minimum-input-length': 0,
+                'step': 0.0001
+            }
+        )
     )
 
     num_epochs = forms.IntegerField(
-
+        min_value=1,
+        max_value=1000,
+        initial=10,
+        required=True,
+        help_text='Number of epochs',
+        widget=forms.NumberInput(
+            attrs={
+                'theme': 'material',
+                'data-minimum-input-length': 0,
+                'step': 1
+            }
+        )
     )
 
-    train_batch_size = forms.IntegerField()
-    predict_batch_size = forms.IntegerField()
+    train_batch_size = forms.IntegerField(
+        min_value=1,
+        max_value=1000,
+        initial=16,
+        required=True,
+        help_text='Size of the batches during training',
+        widget=forms.NumberInput(
+            attrs={
+                'theme': 'material',
+                'data-minimum-input-length': 0,
+                'step': 1
+            }
+        )
+    )
+    predict_batch_size = forms.IntegerField(
+        min_value=1,
+        max_value=1000,
+        initial=32,
+        required=True,
+        help_text='Size of the batches during prediction and evaluation',
+        widget=forms.NumberInput(
+            attrs={
+                'theme': 'material',
+                'data-minimum-input-length': 0,
+                'step': 1
+            }
+        )
+    )
 
     
     
