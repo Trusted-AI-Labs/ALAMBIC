714e868903f5181b4cebff1a6b8defb344045f3d
Charlotte Nachtegael <charlottenachtegael@hotmail.com>
2023-06-07 11:32:07 +0200

    entrypoint for migration

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,10 @@
 echo "Collect static files"
 python manage.py collectstatic --noinput
 
+#Create eventual migrations
+echo "Creating migrations (optional)"
+python manage.py makemigrations
+
 # Apply database migrations
 echo "Apply database migrations"
 python manage.py migrate
